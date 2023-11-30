af698a694c0dbb701d521e6f7b9324bb34a2639a
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-11-30 10:31:52 +0200

    middleware

--- src/middleware.js
+++ src/middleware.js
@@ -4,8 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-      // "/" will be accessible to all users
-      publicRoutes: ["/"]
+      // will be accessible to all users
+      publicRoutes: []
     })
  
 export const config = {
